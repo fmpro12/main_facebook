f49f371425e3c21f858a2255747824e9204cabcb
Yan <yan@example.com>
2018-06-30 10:47:04 -0700

    icons added commit

--- js/post.js
+++ js/post.js
@@ -91,5 +91,20 @@ class UsersService {
   let mainEl = document.querySelector('main');
   new Feed(mainEl, 7);
   
+  fetch('http://127.0.0.1:3000')
+  .then((data) => {
+    data.json()
+      .then((res) => {
+        res.posts.forEach(element => {
+          createPost(element.message);
+        });
+      });
+  });
+
+  function createPost(message) {
+    let postDiv = document.createElement("div");
+    postDiv.innerHTML = message;
+    document.body.appendChild(postDiv);
+  }
   
   
